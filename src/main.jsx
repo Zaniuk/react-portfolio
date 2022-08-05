a7f5db3e7b8524822df7fdeafaa114ebac91785a
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-05 16:46:20 -0300

    Added routes to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import BlogRoute from './routes/Blog.route'
+import AboutRoute from './routes/About.route'
+import ProjectsRoute from './routes/Projects.route'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import './index.scss'
+import NotFoundRoute from './routes/NotFound.route'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/blog' element={<BlogRoute/>}/>
+      <Route path='/about' element={<AboutRoute/>}/>
+      <Route path='/projects' element={<ProjectsRoute/>}/>
+      <Route path='*' element={<NotFoundRoute/>}/>
+    </Routes>
+  </BrowserRouter>
 )
