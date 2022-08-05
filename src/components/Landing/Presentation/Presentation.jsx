99e0f7e126bb7a7e325b5c9fa7df3bd4e0771db9
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-05 20:05:52 -0300

    Added some colors to Presentation.jsx

--- src/components/Landing/Presentation/Presentation.jsx
+++ src/components/Landing/Presentation/Presentation.jsx
@@ -4,11 +4,12 @@ export default function Presentation() {
     return (
         <>
             <div className='presentation-container'>
-                <h1>Hi, I'm Geronimo Zaniuk</h1>
+                <h1>Hi, I'm <span className='accent'>Geronimo Zaniuk</span></h1>
                 <h2>Full-stack web developer</h2>
                 <p>
-                    Natural challenge-seeker, a fast-learner and multidisciplinary person. I'm searching for my first oportunity to work on a software development team.
+                    Natural <span className="accent">challenge-seeker</span>, a <span className="accent">fast-learner</span> and <span className="accent">multidisciplinary</span> person. I'm searching for my first oportunity to work on a software development team.
                 </p>
+                <button>Contact me</button>
             </div>
             
         </>
