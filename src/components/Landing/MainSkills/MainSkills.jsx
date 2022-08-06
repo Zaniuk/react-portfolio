d61737bc20a70ff7dfd679f02dd8728fbe84f57a
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-06 05:41:07 -0300

    added icons to mainskills grid

--- src/components/Landing/MainSkills/MainSkills.jsx
+++ src/components/Landing/MainSkills/MainSkills.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import SkillItem from './SkillItem'
 import './MainSkills.css'
+import { SiSequelize, SiPostgresql, SiMongodb, SiExpress, SiSvelte } from "react-icons/si";
+
+import { FaReact, FaNodeJs, FaPython, FaJsSquare, FaFlask } from "react-icons/fa";
+
 export default function MainSkills() {
-  
+
   return (
     <div className='skills-container' >
-    <ul className='skill-list'>
-      <SkillItem img={'https://i.picsum.photos/id/1023/25/25.jpg?hmac=5WMSLG5AZYwo1D5MIJmtsscTH9r0MWM4QscXZUNsP4o'} altImg={'JavaScript'} skill={'JavaScript'} />
-      <SkillItem img={'https://i.picsum.photos/id/1023/25/25.jpg?hmac=5WMSLG5AZYwo1D5MIJmtsscTH9r0MWM4QscXZUNsP4o'} altImg={'NodeJS'} skill={'NodeJS'} />
-      <SkillItem img={'https://i.picsum.photos/id/1023/25/25.jpg?hmac=5WMSLG5AZYwo1D5MIJmtsscTH9r0MWM4QscXZUNsP4o'} altImg={'ExpressJS'} skill={'ExpressJS'} />
-      <SkillItem img={'https://i.picsum.photos/id/1023/25/25.jpg?hmac=5WMSLG5AZYwo1D5MIJmtsscTH9r0MWM4QscXZUNsP4o'} altImg={'MongoDB'} skill={'MongoDB'} />
-      <SkillItem img={'https://i.picsum.photos/id/1023/25/25.jpg?hmac=5WMSLG5AZYwo1D5MIJmtsscTH9r0MWM4QscXZUNsP4o'} altImg={'PostgreSQL'} skill={'PostgreSQL'} />
-      <SkillItem img={'https://i.picsum.photos/id/1023/25/25.jpg?hmac=5WMSLG5AZYwo1D5MIJmtsscTH9r0MWM4QscXZUNsP4o'} altImg={'Sequelize'} skill={'Sequelize'} />
-      <SkillItem img={'https://i.picsum.photos/id/1023/25/25.jpg?hmac=5WMSLG5AZYwo1D5MIJmtsscTH9r0MWM4QscXZUNsP4o'} altImg={'Python'} skill={'Python'} />
-      <SkillItem img={'https://i.picsum.photos/id/1023/25/25.jpg?hmac=5WMSLG5AZYwo1D5MIJmtsscTH9r0MWM4QscXZUNsP4o'} altImg={'Flask'} skill={'Flask'} />
-    </ul>
-    <div className="skewed-background"></div>
+      <ul className='skill-list'>
+        <SkillItem  icon={<FaJsSquare/>} skill={'JavaScript'} />
+        <SkillItem icon={<FaNodeJs/>} skill={'NodeJS'} />
+        <SkillItem icon={<SiExpress/>} skill={'ExpressJS'} />
+        <SkillItem icon={<SiMongodb/>} skill={'MongoDB'} />
+        <SkillItem icon={<FaReact/>} skill={'ReactJS'} />
+        <SkillItem icon={<SiSvelte/>} skill={'Svelte'} />
+        <SkillItem icon={<SiPostgresql/>} skill={'PostgreSQL'} />
+        <SkillItem icon={<SiSequelize/>} skill={'Sequelize'} />
+        <SkillItem icon={<FaPython/>} skill={'Python'} />
+        <SkillItem icon={<FaFlask/>} skill={'Flask'} />
+      </ul>
+      <div className="skewed-background"></div>
     </div >
   )
 }
