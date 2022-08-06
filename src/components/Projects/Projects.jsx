756139ea19c568906025418e61fd4a8c543a5972
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-06 04:04:42 -0300

    changed in color scheme in project card

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,30 +4,32 @@ import './Projects.scss'
 
 export default function Projects() {
   return (
-    <section className='projects'>
-      <h1>My Projects</h1>
-      <div className="projects-container">
-        <Project
-          projectImg={'https://picsum.photos/200'}
-          projectAlt={'Blog API'}
-          projectName={'Blog API'}
-          projectAbout={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, vitae culpa officiis libero voluptatem'}
-          projectType={'Backend'}
-        />
-        <Project
-          projectImg={'https://picsum.photos/200'}
-          projectAlt={'ABM Operations webpage'}
-          projectName={'ABM Operations webpage'}
-          projectAbout={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, vitae culpa officiis libero voluptatem'}
-          projectType={'Fullstack'}
-        />
-        <Project
-          projectImg={'https://picsum.photos/200'}
-          projectAlt={'The Wine Valley'}
-          projectName={'The Wine Valley'}
-          projectAbout={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, vitae culpa officiis libero voluptatem'}
-          projectType={'Frontend'}
-        />
+    <section className='projects fh'>
+      <div className="projects-wrapper">
+      <h1><span className="accent">My Projects</span></h1>
+        <div className="projects-container">
+          <Project
+            projectImg={'https://picsum.photos/200'}
+            projectAlt={'Blog API'}
+            projectName={'Blog API'}
+            projectAbout={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, vitae culpa officiis libero voluptatem'}
+            projectType={'Backend'}
+          />
+          <Project
+            projectImg={'https://picsum.photos/200'}
+            projectAlt={'ABM Operations webpage'}
+            projectName={'ABM Operations webpage'}
+            projectAbout={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, vitae culpa officiis libero voluptatem'}
+            projectType={'Fullstack'}
+          />
+          <Project
+            projectImg={'https://picsum.photos/200'}
+            projectAlt={'The Wine Valley'}
+            projectName={'The Wine Valley'}
+            projectAbout={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, vitae culpa officiis libero voluptatem'}
+            projectType={'Frontend'}
+          />
+        </div>
       </div>
     </section>
   )
