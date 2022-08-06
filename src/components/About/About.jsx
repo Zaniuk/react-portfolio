85d7ea0600f802f5dbd79edf7e98cc0cc65879f3
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-06 04:14:25 -0300

    WIP About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import './About.scss'
 
 export default function About() {
   return (
-    <section>
-      <h1>About me</h1>
-      <p>My path in technology started when I was 4 years old, when I was given a HP Pavilio 7910 PC. With that machine that accompained me until I reached my teens, I discovered the Internet, and the world of hardware and programming, learning HTML, CSS and Python.
-        Since 2018 I work as freelance and self-employed computer technical support. In 2021 I intend to dedicate myself to fully studying programming, something that I'm passionate about and motivates me every day to continue learning more and more. Today I would say that my main language is JavaScript but I'm open to new languages and technologies.</p>
+    <section className='about fh'>
+      <article>
+        <h1>About me</h1>
+        <p>My path in technology started when I was 4 years old, when I was given a HP Pavilio 7910 PC. With that machine that accompained me until I reached my teens, I discovered the Internet, and the world of hardware and programming, learning HTML, CSS and Python.
+          Since 2018 I work as freelance and self-employed computer technical support. In 2021 I intend to dedicate myself to fully studying programming, something that I'm passionate about and motivates me every day to continue learning more and more. Today I would say that my main language is JavaScript but I'm open to new languages and technologies.</p>
+      </article><article>
+
+        <h1>Why me?</h1>
+        <p>I am a self-taught person, who does not find it difficult to ask or help his colleagues. I tend to take many initiatives, learn quickly, I have knowledge of SCRUM and KANBAN. I am passionate about constantly learning and solving problems and everything I do is done with the passion and ambition to grow as a person and as a professional.</p>
+      </article>
+      <article>
+        <h1>What makes me different?</h1>
+        <p>Something that sets me apart from the rest is my ability to learn, both self-taught and in a more formal way, proof of this is that I have been moving through different fields of technology, such as technical support for computers, sale of technological supplies, and maintenance of industrial machines (CNC).
+          I am confident that my learning abilities, quick adaptability, commitment and ambitions for growth make me the best candidate for your team.</p>
+      </article>
     </section>
   )
 }
