18f1c2b7a9bd785f3219661889bb230b128667fb
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-06 05:40:47 -0300

    added links and icons to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Footer.scss'
+import { SiLinkedin, SiGithub, SiGmail } from "react-icons/si";
 export default function Footer() {
   return (
     <footer>
       <div className="footer-container">
-        <a href="#">LinkedIn</a>
-        <a href="#">GitHub</a>
-        <a href="#">Email</a>
+        <a href="https://www.linkedin.com/in/geronimo-zaniuk/"><span className='linkedin'><SiLinkedin/></span>LinkedIn</a>
+        <a href="https://github.com/Zaniuk"><span className='github'><SiGithub/></span>GitHub</a>
+        <a href="mailto:geronimozaniuk2000@gmail.com"><span className='gmail'><SiGmail/></span>Email</a>
       </div>
     </footer>
   )
