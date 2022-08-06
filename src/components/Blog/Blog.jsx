f51a2103a7940ea669a70113979fb6b56d2b5521
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-06 05:53:13 -0300

    Added message under development in blog

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import './Blog.scss'
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <section className='blog fh'>
+      <h1>Sorry</h1>
+      <h1>This is under development</h1>
+      <div className="go-home">
+        <a href="/"><button>Go Home</button></a>
+      </div>
+    </section>
   )
 }
